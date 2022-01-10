b2ba7e9edc1020af085cd206a0a2c30d4767917b
Ashwini-Rathod <ashwinirathod.f1@gmail.com>
2022-01-10 11:18:31 +0530

    fetching modified entry data using the has provided by live-preview

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Contentstack from "contentstack"
-import { useEffect } from "react";
-// import Contentstack from 'contentstack'
+import { useEffect, useState } from "react";
 
+let apiUrl = `https://api.contentstack.io/v3/content_types/ashwini_live_preview/entries/blt8a69169a533e195a?`
 const Stack = Contentstack.Stack(
  {
   api_key: process.env.api_key,
@@ -37,6 +37,9 @@ function fetchData (contentType, entryId){
 
 export default function Home(props) {
   console.log("PROPS", props)
+  const [entry, setEntry] = useState(props['single_line'])
+  // const {title, single_line, multi_line, paragraph_1, paragraph_2, paragraph_3, file} = entry
+
   useEffect(() => {
     //initializing live preview when the page renders for the first time.
     window.parent.postMessage(
@@ -55,20 +58,34 @@ export default function Home(props) {
   //listening to post messages for the hash value
   window.addEventListener("message", async (e) => {
     console.log("Event:",e) 
-    if(e.data){
-      console.log("DATA". e.data)
-    } 
+    const {data, from, type} = e.data
+    //check for change in data
+    if(data?.hasOwnProperty('hash') && from === "live-preview" && type === "client-data-send"){
+      //making an api call with the hash generated and content type uid
+      let res = await fetch(`${apiUrl}live_preview=${data['hash']}&content_type_uid=${data['content_type_uid']}`, {
+        headers: {
+          api_key: process.env.api_key,
+          access_token: process.env.delivery_token
+        }
+      })
+      let entryData = await res.json()
+      console.log("Fetching changed data", entryData)
+      //changing the states based on the data changed for that particular hash and rendering the same
+      if(entryData){
+        setEntry(entryData.entry)
+      }
+    }
   });
   }, [])
   return (
     <div>
-      <h1>SSR Rendering</h1>
+      <h1>{entry}</h1>
     </div>
   )
 }
 
 export const getServerSideProps = async (context) => {
-  let entry = await fetchData("ashwini_rte", "blt69a28688604b9027");
+  let entry = await fetchData("ashwini_live_preview", "blt8a69169a533e195a");
   return { props: 
     {...entry} 
   }
