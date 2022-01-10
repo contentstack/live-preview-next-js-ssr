29350c22a9319c8ffd0d221451dbad334e1f7ba2
Ashwini-Rathod <ashwinirathod.f1@gmail.com>
2022-01-10 18:35:05 +0530

    env variables tested

--- pages/api/utils.js
+++ pages/api/utils.js
@@ -1,6 +1,15 @@
 import Contentstack from 'contentstack'
 import ContentstackLivePreview from "@contentstack/live-preview-utils";
-
+console.log("params", { 
+  api_key: process.env.api_key,
+  delivery_token: process.env.delivery_token,
+  environment: process.env.environment,
+  live_preview: {
+    management_token: process.env.management_token,
+    enable: true,
+    host: 'api.contentstack.io'
+  }
+})
 export const Stack = Contentstack.Stack(
 { 
   api_key: process.env.api_key,
