1c9441e1b520051da89916179d9d9fa28d8a36bf
Ashwini-Rathod <ashwinirathod.f1@gmail.com>
2022-01-10 18:40:19 +0530

    managemnet token removed

--- pages/api/utils.js
+++ pages/api/utils.js
@@ -15,11 +15,11 @@ export const Stack = Contentstack.Stack(
   api_key: process.env.api_key,
   delivery_token: process.env.delivery_token,
   environment: process.env.environment,
-  live_preview: {
-    management_token: process.env.management_token,
-    enable: true,
-    host: 'api.contentstack.io'
-  }
+  // live_preview: {
+  //   // management_token: process.env.management_token,
+  //   enable: true,
+  //   host: 'api.contentstack.io'
+  // }
 }
 )
 
